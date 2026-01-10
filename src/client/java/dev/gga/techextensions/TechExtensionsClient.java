801efcf749db76fd0ef439ec0536175c3cbfea20
Giorgio Garasto <giorgio@garasto.me>
2026-01-10 11:47:26 +0100

    chore: project scaffolding

---
+++ src/client/java/dev/gga/techextensions/TechExtensionsClient.java
@@ -0,0 +1,10 @@
+package dev.gga.techextensions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TechExtensionsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
